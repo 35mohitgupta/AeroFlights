0856847970ac34aede659382548b5577edf39a8a
mohit <35mohitgupta@gmail.com>
2020-06-01 15:13:28 +0530

    Created the AeroFlights springboot project with all the required dependencies.

---
+++ AeroFlights/src/main/java/com/infy/aeroFlights/AeroFlightsApplication.java
@@ -0,0 +1,13 @@
+package com.infy.aeroFlights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroFlightsApplication.class, args);
+	}
+
+}
---
+++ AeroFlights/src/test/java/com/infy/aeroFlights/AeroFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.aeroFlights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroFlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
